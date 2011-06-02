b07886987c156ede27f0e1541215deccd8fe23d3
Shaform <shaform@gmail.com>
2011-06-02 08:15:57 +0800

    Modified block.

--- block.v
+++ block.v
@@ -2,14 +2,15 @@ module block(
 	input clock, reset,
 	// row: 0~xx, col: 0~yy
 	input [] row, col,
-	// func: 4'b0000=nop, 4'b0011=read, 4'b0101=write, 4'b0111=load(stage)
-	//       4'b1001=dropdown, 4'b1011=pullup, 4'b1101=nop, 4'b1111=nop
+	// func: 4'b0000=nop, 4'b0011=write, 4'b0101=load(stage), 4'b0111=dropdown
+	//       4'b1001=pullup, 4'b1011=noop, 4'b1101=nop, 4'b1111=nop
 	input [3:0] func,
 	// The kind of brick, 0 = empty.
 	// When load, the stage number.
 	input [3:0] in,
 	// The kind of brick, 0 = empty.
 	output [3:0] out
+	output [];
 );
 
 endmodule
