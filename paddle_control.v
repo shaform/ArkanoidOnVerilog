f4a980a1f0a4f1d9140b165ce7bafe205ca3d405
Shaform <shaform@gmail.com>
2011-06-04 12:35:37 +0800

    Completed paddle_control.

--- paddle_control.v
+++ paddle_control.v
@@ -1,34 +1,33 @@
 module paddle_control
-#(
-	parameter PD_SZ = 5,
-)
 (
 	input clock, reset, enable, rotary_event, rotary_right,
 	input [4:0] speed,
 	input [9:0] length,
 	output reg [9:0] paddle_x, paddle_y
 );
-
-localparam MAXX = 639;
-localparam MAXY = 479;
+localparam PD_SZ = 10;
+localparam MAXX = 320;
+localparam MAXY = 480;
+localparam LEFT = 160;
+localparam TOP = 0;
 
 always @(posedge clock)
 begin
 	if (reset) begin
-		paddle_x <= MAXX/2;
-		paddle_y <= MAXY-PD_SZ/2;
+		paddle_x <= LEFT + MAXX/2;
+		paddle_y <= TOP + MAXY-PD_SZ/2;
 	end
 	else if (enable && rotary_event) begin
 		if (rotary_right) begin
-			if (paddle_x + length/2 + speed < MAXX)
+			if (paddle_x + length/2 + speed < LEFT + MAXX)
 				paddle_x <= paddle_x + length/2 + speed;
 			else
-				paddle_x <= MAXX - length/2;
+				paddle_x <= LEFT + MAXX - length/2;
 		end else begin
-			if (paddle_x > length/2 + speed)
+			if (paddle_x > LEFT + length/2 + speed)
 				paddle_x <= paddle_x - length/2 - speed;
 			else
-				paddle_x <= length/2;
+				paddle_x <= LEFT + length/2;
 		end
 	end
 end
