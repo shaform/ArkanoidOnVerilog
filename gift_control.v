e13412308504ef898b79b488a9518530d9b88594
Huiqin <huichinh@bsd3.cs.nctu.edu.tw>
2011-06-10 21:21:03 +0800

    I'm not sure when should I assign the o_x & o_y inside.
     It's still waiting for debuging XD

--- gift_control.v
+++ gift_control.v
@@ -2,7 +2,7 @@ module gift_control(
 	input clock, reset, lost,
 	input [9:0] paddle_x, paddle_y,
 	// The gift position
-	output kind,
+	output [2:0]kind,
 	output [9:0] o_x, o_y,
 	output active
 );
@@ -16,9 +16,36 @@ localparam SOT = 3'b101;
 localparam DRP = 3'b110;
 localparam MUL = 3'b111;
 
-rand r_gen(clock, reset)
+wire [31:0] rand_num;
+wire [4:0] num; // 25% to get gift
+
+rand r_gen(clock, reset, rand_num)
+
+assign num = rand_num%32;
 
 
-always @(posedge 
+// set active value
+always @ (posedge clock)  
+begin
+	if (reset) 
+		active = 1'b0;
+	else if (num[4:3] == 2'b00) 
+		active = 1'b1;
+	    
+end
+
+always @ (posedge clock)
+begin
+	case(num)
+	5'b00000: kind = INC;
+	5'b00001: kind = DEC;
+	5'b00010: kind = SPU;
+	5'b00011: kind = SPD;
+	5'b00100: kind = HID;
+	5'b00101: kind = SOT;
+	5'b00110: kind = DRP;
+	5'b00111: kind = MUL;
+	default:
+end
 
 endmodule
