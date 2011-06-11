201142963b147bd2ce5a1b3317de8a8ba72dd0cb
Shaform <shaform@gmail.com>
2011-06-11 09:59:42 +0800

    Updated gift_control.

--- gift_control.v
+++ gift_control.v
@@ -1,59 +1,42 @@
 module gift_control(
-	input clock, reset, enable,
+	input clock, reset, enable, hit,
 	// The gift position
 	input [9:0] in_x, in_y,
 	output [2:0] kind,
 	output [9:0] x, y,
-	output active
+	output active,
+	// control signal
+	output paddle_size, paddle_speed, give_ball, ball_size, ball_display, get_shot, drop_block
+
 );
 `include "def.v"
-localparam INC = 3'b000;
-localparam DEC = 3'b001;
-localparam SPU = 3'b010;
-localparam SPD = 3'b011;
-localparam HID = 3'b100;
-localparam SOT = 3'b101;
-localparam DRP = 3'b110;
-localparam MUL = 3'b111;
+localparam CHP = 3'b000; // change paddle size
+localparam SPP = 3'b001; // change paddle speed
+localparam GBL = 3'b010; // give one ball
+localparam SPB = 3'b011; // change ball speed
+localparam CHB = 3'b100; // change ball size
+localparam HID = 3'b101; // change ball display
+localparam SOT = 3'b110; // get shot capacity
+localparam DRP = 3'b111; // drop the blocks
 
-wire [31:0] rand_num;
-wire [4:0] num; // 25% to get gift
+wire [31:0] num;
 
 wire [UBIT-1:0] cnt;
 assign rst = reset || cnt >= UNIT;
 counter #(UBIT) cnt0(clock, rst, enable, cntx);
 
-rand r_gen(clock, reset, rand_num)
-
-assign num = rand_num%32;
+rand r_gen(clock, reset, num)
 
 
 // set active value
 always @ (posedge clock)  
 begin
-	if (reset || y >= TOP+MAXY) 
-		active = 1'b0;
-	else if (enable && num[4:3] == 2'b00) 
-		active = 1'b1;
+	if (reset || y >= TOP+MAXY || hit)
+		active <= 1'b0;
+	else if (enable && num[15:14] == 2'b00) 
+		active <= 1'b1;
 	    
 end
-
-always @ (posedge clock)
-begin
-	case(num)
-		5'b00000: kind = INC;
-		5'b00001: kind = DEC;
-		5'b00010: kind = SPU;
-		5'b00011: kind = SPD;
-		5'b00100: kind = HID;
-		5'b00101: kind = SOT;
-		5'b00110: kind = DRP;
-		5'b00111: kind = MUL;
-		default: kind = 3'bxxx;
-	default:
-end
-
-
 
 
 always @(posedge clock)
@@ -63,6 +46,30 @@ begin
 	end else if (enable) begin
 		x <= in_x;
 		y <= in_y;
+		kind <= rand_num[13:11];
+	end
+end
+
+always @(*)
+begin
+	if (active && hit) begin
+		if (kind == CHP) paddle_size = 1'b1;
+		if (kind == SPP) paddle_speed = 1'b1;
+		if (kind == GBL) give_ball = 1'b1;
+		if (kind == SPB) ball_speed = 1'b1;
+		if (kind == CHB) ball_size = 1'b1;
+		if (kind == HID) ball_display = 1'b1;
+		if (kind == SOT) get_shot = 1'b1;
+		if (kind == DRP) drop_block = 1'b1;
+	end else beign
+		paddle_size = 1'b0;
+		paddle_speed = 1'b0;
+		give_ball = 1'b0;
+		ball_speed = 1'b0;
+		ball_size = 1'b0;
+		ball_display = 1'b0;
+		get_shot = 1'b0;
+		drop_block = 1'b0;
 	end
 end
 
