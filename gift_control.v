5ffe852a003741f763119d0b7fbc2bb3eaf6a716
Shaform <shaform@gmail.com>
2011-06-11 09:36:57 +0800

    Updated gift_control.

--- gift_control.v
+++ gift_control.v
@@ -1,12 +1,12 @@
 module gift_control(
-	input clock, reset, lost,
-	input [9:0] paddle_x, paddle_y,
+	input clock, reset, enable,
 	// The gift position
-	output [2:0]kind,
-	output [9:0] o_x, o_y,
+	input [9:0] in_x, in_y,
+	output [2:0] kind,
+	output [9:0] x, y,
 	output active
 );
-localparam PD_SZ = 10;
+`include "def.v"
 localparam INC = 3'b000;
 localparam DEC = 3'b001;
 localparam SPU = 3'b010;
@@ -19,6 +19,10 @@ localparam MUL = 3'b111;
 wire [31:0] rand_num;
 wire [4:0] num; // 25% to get gift
 
+wire [UBIT-1:0] cnt;
+assign rst = reset || cnt >= UNIT;
+counter #(UBIT) cnt0(clock, rst, enable, cntx);
+
 rand r_gen(clock, reset, rand_num)
 
 assign num = rand_num%32;
@@ -27,9 +31,9 @@ assign num = rand_num%32;
 // set active value
 always @ (posedge clock)  
 begin
-	if (reset) 
+	if (reset || y >= TOP+MAXY) 
 		active = 1'b0;
-	else if (num[4:3] == 2'b00) 
+	else if (enable && num[4:3] == 2'b00) 
 		active = 1'b1;
 	    
 end
@@ -37,15 +41,29 @@ end
 always @ (posedge clock)
 begin
 	case(num)
-	5'b00000: kind = INC;
-	5'b00001: kind = DEC;
-	5'b00010: kind = SPU;
-	5'b00011: kind = SPD;
-	5'b00100: kind = HID;
-	5'b00101: kind = SOT;
-	5'b00110: kind = DRP;
-	5'b00111: kind = MUL;
+		5'b00000: kind = INC;
+		5'b00001: kind = DEC;
+		5'b00010: kind = SPU;
+		5'b00011: kind = SPD;
+		5'b00100: kind = HID;
+		5'b00101: kind = SOT;
+		5'b00110: kind = DRP;
+		5'b00111: kind = MUL;
+		default: kind = 3'bxxx;
 	default:
+end
+
+
+
+
+always @(posedge clock)
+begin
+	if (active) begin
+		y <= y+1;
+	end else if (enable) begin
+		x <= in_x;
+		y <= in_y;
+	end
 end
 
 endmodule
